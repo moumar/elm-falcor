ff9017795eb69c958ce4b9587c2ac2bfa5b908f6
Guillaume Pierronnet <guillaume@dataaddict.fr>
2016-04-01 13:17:50 +0200

    added load* methods

--- Falcor.elm
+++ Falcor.elm
@@ -1,10 +1,12 @@
 module Falcor where
 
-import Json.Encode as Json
+import Json.Encode
 import Task exposing (Task)
+import Effects exposing (Never)
 -- import Debug exposing (log)
-import Native.Falcor
 import String
+import Json.Decode as Json exposing ((:=))
+import Native.Falcor
 
 type alias Model = Json.Value
 
@@ -21,7 +23,7 @@ type Path
   | PathStrings (List String)
   | Range Int Int
 
-convertPaths : List Path -> List Json.Value
+convertPaths : List Path -> List Json.Encode.Value
 convertPaths paths =
   let _ = ""
   in
@@ -29,13 +31,13 @@ convertPaths paths =
     (\path ->
       case path of
         PathList lst ->
-          Json.list <| convertPaths lst
+          Json.Encode.list <| convertPaths lst
         PathString str ->
-          Json.string str
+          Json.Encode.string str
         PathStrings arr ->
-          Json.list <| List.map Json.string arr
+          Json.Encode.list <| List.map Json.Encode.string arr
         Range from to ->
-          Json.object [ ("from", Json.string <| toString <| from), ("to", Json.string <| toString <| to) ]
+          Json.Encode.object [ ("from", Json.Encode.string <| toString <| from), ("to", Json.Encode.string <| toString <| to) ]
     ) paths
 
 {-
@@ -53,7 +55,7 @@ createModel : Options -> Model
 createModel = Native.Falcor.createModel
 
 get : Model -> List Path -> Task err Json.Value
-get model paths = Native.Falcor.get model (convertPaths paths |> Json.list)
+get model paths = Native.Falcor.get model (convertPaths paths |> Json.Encode.list)
 
 setValue : Model -> List String -> String -> Task err ()
 setValue model path value = Native.Falcor.setValue model (listToJs path) value
@@ -69,7 +71,7 @@ hashToList lst =
   |> List.map snd
 
 listToJs : List String -> Json.Value
-listToJs = List.map Json.string >> Json.list
+listToJs = List.map Json.Encode.string >> Json.Encode.list
 
 {-
 load : (List String) -> Json.Decoder a -> Task Error a
@@ -103,3 +105,65 @@ prefixPath prefix lst =
          )
     )
     lst
+
+
+listDecoder : Json.Decoder a -> Json.Decoder (List a)
+listDecoder decoder =
+  Json.oneOf
+    [ Json.keyValuePairs decoder |> Json.map hashToList
+    , Json.succeed []
+    ]
+
+load : Model -> List String -> List Path -> Json.Decoder a -> Task Never (Result String a)
+load falcorModel prefix paths decoder =
+  get
+    falcorModel
+    (prependPath prefix paths [])
+  |> Task.toResult
+  |> Task.map
+    (\resultJsonVal ->
+      Result.andThen
+        resultJsonVal
+        (\v -> Json.decodeValue (Json.at prefix decoder) v)
+    )
+
+loadCollection : Model -> List String -> List Path -> Json.Decoder a -> Int -> Int -> Task Never (List a)
+loadCollection falcorModel prefix paths decoder from to =
+    get
+      falcorModel
+      (prependPath prefix paths [ Range from to ])
+    |> Task.toMaybe
+    |> Task.map
+         (\maybeJsonVal ->
+            Maybe.andThen
+              maybeJsonVal
+              (\v ->
+                 Result.toMaybe <|
+                   Json.decodeValue
+                   (Json.at prefix (listDecoder decoder))
+              v
+        ) |> Maybe.withDefault []
+      )
+
+loadCollection2 : Model -> List String -> List Path -> Json.Decoder a -> Json.Decoder b -> Int -> Int -> Task Never (List (a, b))
+loadCollection2 falcorModel prefix paths decoder1 decoder2 from to =
+    get
+      falcorModel
+      (prependPath prefix paths [ Range from to ])
+    |> Task.toResult
+    |> Task.map
+      (\resultJsonVal ->
+        Result.andThen
+          resultJsonVal
+          (\v ->
+            let
+              dec decoder =
+                Json.decodeValue
+                  (Json.at prefix (Json.keyValuePairs decoder |> Json.map hashToList))
+                  v
+            in
+              Result.map2 (,) (dec decoder1) (dec decoder2)
+              |> Result.map (\(lst1, lst2) -> List.map2 (,) lst1 lst2)
+          )
+          |> Result.toMaybe |> Maybe.withDefault []
+      )
